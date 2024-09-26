99f35630f896cd84bc84546361fb144f596455d3
mcarnival <michcarnival@gmail.com>
2024-09-26 00:29:14 -0400

    change int

--- train.py
+++ train.py
@@ -13,8 +13,8 @@ def evaluate(model, data_loader, device):
     - data_loader: A Pytorch data loader object
     """
     
-    y_true = np.array([], dtype=np.int)
-    y_pred = np.array([], dtype=np.int)
+    y_true = np.array([], dtype=int)
+    y_pred = np.array([], dtype=int)
     
     with torch.no_grad():
         for data in data_loader:
